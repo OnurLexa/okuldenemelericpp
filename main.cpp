c24ceee51a9a02a8dc1b16854fa57c19dfd6d3c9
Onur <lexa1337onur@proton.me>
2024-10-12 01:06:00 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,108 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Merhaba"<<" "<<"Dünya"<<" "<<"Nasılsın";
+    return 0;
+}
+*/
+
+/*
+int main(){
+    cout<<"Toplama işlemi sonucu"<<" "<<10+5<<" "<<"Bölme işlemi sonucu"<<" "<<10/2; // toplama ve carpma yapar
+    cout<<"Çarpma işlemi sonucu"<<" "<<10*10<<" "<<"Çıkarma işlemi sonucu"<<" "<<100/2; // bölme ve cıkarma yapar
+    
+    return 0;
+}
+*/
+
+// 2. bolum
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"toplama islemi = "<<10+5<<endl; // "endl" satırları ayırır
+    cout<<"bölme islemi   = "<<10/5<<endl;
+    cout<<"carpma islemi  = "<<10*5<<endl;
+    cout<<"cıkarma islemi = "<<10-5<<endl;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main(){
+string isim, soyisim;
+int yas=10;
+
+isim=" mehmet";
+soyisim=" atıcı";
+
+cout<<"Merhaba"<<isim<<soyisim<<" "<<"Yaşın doğru mu? "<<yas;
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int main(){
+    string isim, soyisim;
+    int yas=10;
+    
+    cout<<"lütfen bir isim gir: ";
+    cin>>isim;
+    cout<<isim;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int main(){
+    string isim, soyisim, tc, dogumtarih, boy;
+    cout<<"İsim girin: "<<endl;
+    cin>>isim;
+    cout<<"Soyisim girin: "<<endl;
+    cin>>soyisim;
+    cout<<"TC Kimlik girin: "<<endl;
+    cin>>tc;
+    cout<<"Doğum Tarihi girin"<<endl;
+    cin>>dogumtarih;
+    cout<<"Boyunuzu girin"<<endl;
+    cin>>boy;
+    
+    cout<<"Merhaba: "<<isim<<endl<<" Soyisim Gir: "<<soyisim<<endl<<" Lütfen TC girin: "<<tc<<endl<<" Doğum tarihi gir: "<<dogumtarih<<endl<<" Boyunu gir"<<boy;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int main(){
+    
+    string ad, soyad, dogumtarihi;
+    double boy;
+    int tc;
+    cout<<"adınızı girin: ";
+    cin>>ad;
+    cout<<"soyadınızı girin: ";
+    cin>>soyad;
+    cout<<"doğum tarihinizi girin: ";
+    cin>>dogumtarihi;
+    cout<<"boyunu gir: ";
+    cin>boy;
+    cout<<"tc girin";
+    cin>>tc;
+    
+    cout<<"merhaba "<<ad<<" "<<soyad<<" Doğum tarihin: "<<dogumtarihi<<endl;
+    cout<<"boyunuz: "<<boy<<"Tcniz"<tc;
+}
+*/
+
+
+
+
